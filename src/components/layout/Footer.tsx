43d2a2cbb1d3d9c5b1e84f08181465a345c3f322
aipioneers-me <claus@aipioneers.me>
2025-07-26 00:18:43 +0200

    fixing info in index.html

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
             <div className="flex flex-col md:flex-row items-center border border-accent-blue/30 rounded-lg p-4 bg-white/60 w-full justify-between">
               <div className="flex-1 text-right md:pr-3">
                 <span className="block text-base text-charcoal font-medium">If you find our work valuable, we warmly welcome  </span>
-                <span className="block text-base text-charcoal font-medium">any support — it helps us keep the program free.</span>
+                <span className="block text-base text-charcoal font-medium">any support, it helps us keep the program free.</span>
               </div>
               <div className="flex-shrink-0 md:pl-6 mt-3 md:mt-0">
                 <a href="https://donorbox.org/ai-pioneers" className="inline-block" target="_blank" rel="noopener noreferrer">
